1e1cd04ebb58ba1abd07fea23f701cb0191981c7
Haider Abbas <haiderawan660@gmail.com>
2023-11-02 14:25:01 +0500

    fix map issue rent page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   env: {
     API_HOST: process.env.API_HOST,
     GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY,
-    FACEBOOK_LINK:process.env.FACEBOOK_LINK,
-    INSTAGRAM_LINK:process.env.INSTAGRAM_LINK,
-    TWITTER_LINK:process.env.TWITTER_LINK,
-    LINKEDIN_LINK:process.env.LINKEDIN_LINK,
-    YOUTUBE_LINK:process.env.YOUTUBE_LINK,
-    TIKTOK_LINK:process.env.TIKTOK_LINK,
+    FACEBOOK_LINK: process.env.FACEBOOK_LINK,
+    INSTAGRAM_LINK: process.env.INSTAGRAM_LINK,
+    TWITTER_LINK: process.env.TWITTER_LINK,
+    LINKEDIN_LINK: process.env.LINKEDIN_LINK,
+    YOUTUBE_LINK: process.env.YOUTUBE_LINK,
+    TIKTOK_LINK: process.env.TIKTOK_LINK,
   },
 };
 
