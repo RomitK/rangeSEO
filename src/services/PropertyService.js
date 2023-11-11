aa85ffff2b653725d8d984c328d736ff53b70c82
Haider Abbas <haiderawan660@gmail.com>
2023-11-11 19:58:23 +0500

    swiper auto play & api changes

--- src/services/PropertyService.js
+++ src/services/PropertyService.js
@@ -1,10 +1,6 @@
 import useSWR from "swr";
 export const useGetAllPropertyData = () => {
-  const {
-    data: propertiesData,
-    error,
-    mutate,
-  } = useSWR(`/properties`);
+  const { data: propertiesData, error, mutate } = useSWR(`/properties`);
   return { propertiesData: propertiesData?.data, propertiesDataMutate: mutate };
 };
 export const useGetSinglePropertyData = (slug) => {
@@ -14,4 +10,24 @@ export const useGetSinglePropertyData = (slug) => {
     mutate,
   } = useSWR(slug ? `/properties/${slug}` : null);
   return { propertyData: propertyData?.data, propertyDataMutate: mutate };
+};
+
+export const useGetAccommodations = () => {
+  const { data: accommodations, error, mutate } = useSWR(`/accommodations`);
+  return { accommodations: accommodations?.data, accommodationsMutate: mutate };
+};
+
+export const useGetCommunities = () => {
+  const { data: communities, error, mutate } = useSWR(`/communities`);
+  return { communities: communities?.data, communitiesMutate: mutate };
+};
+
+export const useGetAmenities = () => {
+  const { data: amenities, error, mutate } = useSWR(`/amenities`);
+  return { amenities: amenities?.data, amenitiesMutate: mutate };
+};
+
+export const useGetProperties = (params) => {
+  const { data: properties, error, mutate } = useSWR(`/properties?` + params);
+  return { propertiesData: properties?.data, propertiesMutate: mutate };
 };
