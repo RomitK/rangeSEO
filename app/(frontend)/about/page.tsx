6886761accce1d62bf80b69914df55496960afa8
muhammadashiq <muhammadashiq@team.softpers.com>
2023-11-12 00:27:35 +0500

    management page done

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -12,15 +12,18 @@ function About(){
                          <p className="fs-14 text-secondary mb-4">
                              Lorem Ipsum, automated, and futuristic approach to the Dubai property market.
                              Our state-of-the-art marketing ensures a competitive edge with modern and 
-                             advanced strategies. Experience bespoke services tailored to deliver accurate results and ultimate customer satisfaction.
+                             advanced strategies. Experience bespoke services tailored to deliver accurate 
+                             results and ultimate customer satisfaction.
                          </p>
                          <p className="fs-14 text-secondary mb-5">
-                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
                              eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
                              sunt in culpa qui officia deserunt mollit anim id est laborum.
+                             
+
                          </p>
 
                          <div className="row">
--- app/(frontend)/management/page.tsx
+++ app/(frontend)/management/page.tsx
@@ -1,7 +1,94 @@
+"use client";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Swiper as SwiperType } from "swiper";
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import "@/public/css/management-styles.css";
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+
+
+
+
 function Management(){
+    const PropertySwiperRef = useRef<SwiperCore>();
+  const testimonialSwiperRef = useRef<SwiperCore>();
     return (
         <>
-        Management
+          <div className="container">
+                <div className="PagiList mb-4">
+                    <span className="pagiText">Meet the Leader</span>
+                    <span className="pagiText">Meet the Leader</span>
+                </div>
+                <section className="section ">
+                        <div className="row align-items-center">
+                                <div className="col-md-8">
+                                    <h4 className="sctionMdTitle text-primary">NITIN CHOPRA</h4>
+                                    <span className="ceoText">CEO/Founder</span>
+                                    <p className="fs-12 text-secondary mb-4">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+                                        amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                                        qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                    <p className="fs-12 text-secondary mb-4">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                                    </p>
+                                </div> 
+                                <div className="col-md-4">
+                                     <img src="images/team/team-img-1.png" className="leaderImg" />
+                                </div>
+                            </div>
+                </section>
+                <section  className="section carouselSection">
+                     <video className="sectionImgVideo" autoPlay loop muted preload="metadata" poster="/images/services/service-header.webp">
+                          <source src="/videos/services.mp4" type="video/mp4" />
+                          <source src="/videos/services.mp4" type="video/mov" />
+                          Sorry, your browser doesn't support videos.
+                     </video>
+                </section>
+                <section className='section'>
+                <h4 className="sctionMdTitle text-primary mb-5">AWARD WINNING TEAM</h4>
+
+                        <Swiper  
+                        modules={[Navigation]}
+                        onSwiper={(swiper) => {
+                          testimonialSwiperRef.current = swiper;
+                        }}
+                        slidesPerView={3}  navigation={true}  className="mySwiper multiItemsSlider">
+                                        <SwiperSlide>
+                                                <img src="images/blogs/blog5.png" className='awardsImg'/>
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img src="images/blogs/blog2.png" className='awardsImg'/>
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img src="images/blogs/blog3.png" className='awardsImg'/>
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img src="images/blogs/blog4.png" className='awardsImg'/>
+                                        </SwiperSlide>
+                                       
+                               
+                        </Swiper>
+                      
+                </section>
+          </div>
         </>
     );
 }
