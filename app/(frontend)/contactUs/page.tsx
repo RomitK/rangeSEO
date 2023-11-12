000ceb574f956c06fc06621d7a9e75b880a73da6
aqsa <aqsa@xpertise.ae>
2023-11-12 16:41:24 +0400

    add content of about, contactus,management

--- app/(frontend)/contactUs/page.tsx
+++ app/(frontend)/contactUs/page.tsx
@@ -11,9 +11,8 @@ function ContactUs(){
                    <div className="container">
                         <h4 className="sctionMdTitle text-primary text-center ">CONTACT US</h4>
                         <p className="fs-12 text-secondary mxWdtext mb-5">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            
+                        Connect with Range for unparalleled real estate solutions. From inquiries to collaborations, our team is ready to assist. Reach out through the form below or stay updated on industry insights through our social channels. Let's transform your property goals into reality – contact Range today.
                         </p>
                    </div>
            </section>
@@ -32,7 +31,7 @@ function ContactUs(){
                                  <div className="col-md-4">
                                        <div className="contactBox bdr">
                                             <h5>Email</h5>
-                                            <a href="#" className="textFlexBar">
+                                            <a href="mailto:sales@range.ae" className="textFlexBar">
                                                 <img src="/images/icons/mail-icon.png" className="contact-icon" />
                                                 <p>sales@range.ae</p>
                                             </a>
@@ -40,7 +39,7 @@ function ContactUs(){
                                  </div>
                                  <div className="col-md-4">
                                        <div className="contactBox">
-                                            <h5>Call Us</h5>
+                                            <h5>WhatsApp</h5>
                                             <a href="#" className="textFlexBar">
                                                 <img src="/images/icons/whatsapp-icon.png" className="contact-icon" />
                                                 <p>+971 56 1234567</p>
@@ -58,7 +57,7 @@ function ContactUs(){
                               <div className="addressBox">
                                      <h4>Request a Call back</h4>
                                      <p>
-                                       Please fill up the form
+                                     Please fill up the form
                                      </p>
                                      <div className="addressBoxContent">
                                           <div className="row g-3">
