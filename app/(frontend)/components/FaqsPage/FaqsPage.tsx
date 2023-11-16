a853cab18996ba56e63d5e7b2e9343ebc9011cf1
aqsa <aqsa@xpertise.ae>
2023-11-16 13:17:21 +0400

    add faqs page with dynamic data

--- app/(frontend)/components/FaqsPage/FaqsPage.tsx
+++ app/(frontend)/components/FaqsPage/FaqsPage.tsx
@@ -1,21 +1,77 @@
 "use client";
 import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Swiper as SwiperType } from "swiper";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import Link from "next/link";
 import { useGetAllFaqsData } from "@/src/services/FaqService";
+import parse from "html-react-parser";
 
 function FaqsPage(){
     const { faqsData } = useGetAllFaqsData();
-    console.log(faqsData)
+    const [activeIndex, setActiveIndex] = useState(0);
     return (
-        <h1>
-            FAQS
-        </h1>
+        <>
+        <section className="faqSection">
+        <div className="container">
+          <h4 className="sctionMdTitle text-primary ">FAQS</h4>
+
+          <div className="accordion" id="FAQAccordion">
+            {faqsData &&
+              faqsData?.map((faq, index) => {
+                return (
+                  <div className="accordion-item" key={index + "faq"}>
+                    <button
+                      className={`accordion-button ${activeIndex != index ?' collapsed':''} `}
+                      data-bs-toggle="collapse"
+                      data-bs-target={"#faqCollapse-" + index}
+                      aria-expanded={activeIndex == index ? true : false}
+                      onClick={() => {
+                        setActiveIndex(index);
+                      }}
+                    >
+                      {faq.question}
+                    </button>
+                    <div
+                      id={"faqCollapse-" + index}
+                      className={`accordion-collapse collapse  ${
+                        activeIndex == index ? " show" : ""
+                      } `}
+                      data-bs-parent="#FAQAccordion"
+                    >
+                      <div className="accordion-body">
+                        <p className="fs-14 text-secondary">
+                          {faq && faq.answer && parse(faq?.answer ?? "")}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </section>
+      <section className="sectionBanner">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <h2 className="bnrTitle">Still need help?</h2>
+              <p className="fs-12 text-secondary">
+                Click on the blue round button at the bottom right corner of
+                this page. You can <br />
+                also email our support team at{" "}
+                <a href="#" className="fs-12">
+                  info@range.ae
+                </a>
+              </p>
+            </div>
+            <div className="col-md-4">
+             
+              <Link className="fillBtn contactBtn btn" href="/contactUs">
+                Contact Us
+            </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+        </>
     );
 }
 export default FaqsPage;
--- app/(frontend)/faqs/page.tsx
+++ app/(frontend)/faqs/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { SWRProvider } from "@/app/swr-provider";
 import FaqsPage from "../components/FaqsPage/FaqsPage";
+import "@/public/css/faq-styles.css";
 
 function Faqs() {
   return (
