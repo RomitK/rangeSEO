78c71213608a37e3ecb4ff976c518ea59aac24fa
Haider Abbas <haiderawan660@gmail.com>
2023-11-30 17:21:27 +0500

    build fix

--- app/(frontend)/components/UI/CustomSelectToggle.js
+++ app/(frontend)/components/UI/CustomSelectToggle.js
@@ -2,19 +2,19 @@ import React from "react";
 import classes from "./CustomSelectToggle.module.css";
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-    <a
-        href="/"
-        className={`dt text-truncate form-control form-select ${classes.customDropdown}`}
-        ref={ref}
-        style={{ textDecoration: "none" }}
-        onClick={(e) => {
-            e.preventDefault();
-            onClick(e);
-        }}
-    >
-        {/* Customize the dropdown icon here */}
-        {children}
-    </a>
+  <a
+    href="/"
+    className={`dt text-truncate form-control form-select ${classes.customDropdown}`}
+    ref={ref}
+    style={{ textDecoration: "none" }}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {/* Customize the dropdown icon here */}
+    {children}
+  </a>
 ));
-
+CustomToggle.displayName = "CustomToggle";
 export default CustomToggle;
