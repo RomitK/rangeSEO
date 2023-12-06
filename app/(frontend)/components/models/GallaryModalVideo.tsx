92f576b5434d0fd84fd177f34044948ac5ba5b24
aqsa <aqsahussainphp@gmail.com>
2023-12-06 11:35:01 +0400

    add property youtube link

--- app/(frontend)/components/models/GallaryModalVideo.tsx
+++ app/(frontend)/components/models/GallaryModalVideo.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 
 import "@/public/css/gallery-modal-styles.css";
-
+import { Pagination, Navigation } from "swiper/modules";
+import ReactPlayer from 'react-player/lazy'
 function GallaryModalVideo(props) {
   return (
     <>
@@ -12,7 +13,7 @@ function GallaryModalVideo(props) {
         data-bs-target="#gallaryModalVideo"
       >
         <i className="fa fa-play" aria-hidden="true"></i>
-        PLAY VIDEO
+       PLAY VIDEO
       </button>
 
       <div
@@ -24,33 +25,39 @@ function GallaryModalVideo(props) {
       >
         <div className="modal-dialog  modal-dialog-centered modal-lg modalBookMeet ">
           <div className="modal-content">
-            <div className=" border-0 justify-content-end p-0">
+            <div className="modal-header border-0 justify-content-end p-1">
               <button
                 type="button"
-                className="bg-transparent border-0 mdlCloseBtn"
+                className="bg-transparent border-0"
                 data-bs-dismiss="modal"
                 aria-label="Close"
-                >
+              >
                 <i className="bi bi-x-circle text-primary"></i>
               </button>
             </div>
 
             {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
-            <div className="modal-body  p-0 rounded-1">
+            <div className="modal-body  p-0 rounded-1 m-2">
               <div className="row g-0">
                 <div className="col-12 col-lg-12 col-md-12 descricalenderCol">
-                  <video
-                    className="d-block w-100 galleryMdlvideo"
-                    autoPlay
-                    loop
-                    muted
-                    preload="metadata"
-                    poster="/images/services/service-header.webp"
-                  >
-                    <source src={props.video} type="video/mp4" />
-                    <source src={props.video} type="video/mov" />
-                    Sorry, your browser doesn't support videos.
-                  </video>
+                 
+
+                <div className="d-block w-100">
+                <ReactPlayer className='player-wrapper'
+                 width='100%'
+                onEnded={props.onEnd}
+                config={{
+                    youtube: {
+                        playerVars: {
+                            autoplay: 1,
+                            controls: 1
+                        }
+                    }
+                }}
+                url='https://www.youtube.com/watch?v=-6jlrq7idl8&list=PLiPk70af-7kf5A4vVxIWXr1yMaaoBTOb4' />
+
+                </div>
+                  
                 </div>
               </div>
             </div>
