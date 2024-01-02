c931a61a48c057abb7892b7d652bb25285bec5ce
aqsa <aqsahussainphp@gmail.com>
2024-01-02 12:26:55 +0400

    mobile view looking portion update

--- app/(frontend)/components/LookingFor/LookingFor.tsx
+++ app/(frontend)/components/LookingFor/LookingFor.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Swiper as SwiperType } from "swiper";
+import "swiper/swiper-bundle.css";
 
 function LookingFor() {
+  const swiperRef = useRef<SwiperType>();
   return (
     <>
       <section className="my-5">
@@ -71,7 +77,7 @@ function LookingFor() {
               </div>
             </div>
           </div>
-          <div className="d-flex circleCardsRow">
+          <div className="d-flex circleCardsRow lookingForDesktopItem">
             {/* Circle box Start */}
             <div className="proCircleCard">
               <Link className="proCircleBox" href="/properties">
@@ -167,7 +173,154 @@ function LookingFor() {
             </div>
             {/* Circle box End */}
           </div>
+          <div className="lookingForMobileItem">
+          <Swiper
+                    slidesPerView={1}
+                    spaceBetween={10}
+                    navigation={{
+                      nextEl: ".swiper-button-next",
+                      prevEl: ".swiper-button-prev",
+                    }}
+                    loop={true}
+                    breakpoints={{
+                      640: {
+                        slidesPerView: 2,
+                        spaceBetween: 50,
+                      },
+                      768: {
+                        slidesPerView: 2,
+                        spaceBetween: 50,
+                      },
+                      1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 50,
+                      },
+                    }}
+                    modules={[Navigation]}
+                    onBeforeInit={(swiper) => {
+                      swiperRef.current = swiper;
+                    }}
+                    className="swiper mb-2"
+                  >
+                    <SwiperSlide className="lookingMobileCard">
+                      <div className="proCircleCard mb-2" >
+                        <Link className="proCircleBox lookingSingleMobileCard" href="/properties">
+                          <div className="circleContent">
+                            <img
+                              src="/images/icons/interest1.png"
+                              className="proCricleImg"
+                              alt="range"
+                              width="80px"
+                            />
+                            <p className="text">Buying/Renting</p>
+                          </div>
+                        </Link>
+                        <div className="nbrCircle">
+                          <h6>01</h6>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="lookingMobileCard">
+                      {/* Circle box Start */}
+                      <div className="proCircleCard mb-2">
+                        <Link className="proCircleBox lookingSingleMobileCard" href="/sell">
+                          <div className="circleContent">
+                            <img
+                              src="/images/icons/interest2.png"
+                              className="proCricleImg"
+                              alt="range"
+                              width="80px"
+                            />
+                            <p className="text">Selling</p>
+                          </div>
+                        </Link>
+                        <div className="nbrCircle">
+                          <h6>02</h6>
+                        </div>
+                      </div>
+                      {/* Circle box End */}
+                    </SwiperSlide>
+                    <SwiperSlide className="lookingMobileCard">
+                      {/* Circle box Start */}
+                      <div className="proCircleCard mb-2 ">
+                        <Link className="proCircleBox lookingSingleMobileCard" href="/services">
+                          <div className="circleContent">
+                            <img
+                              src="/images/icons/interest2.png"
+                              className="proCricleImg"
+                              alt="range"
+                              width="80px"
+                            />
+                            <p className="text">Property Management</p>
+                          </div>
+                        </Link>
+                        <div className="nbrCircle">
+                          <h6>03</h6>
+                        </div>
+                      </div>
+                      {/* Circle box End */}
+                    </SwiperSlide>
+                    <SwiperSlide className="lookingMobileCard">
+                     {/* Circle box Start */}
+                    <div className="proCircleCard mb-2 ">
+                      <Link className="proCircleBox lookingSingleMobileCard" href="/services">
+                        <div className="circleContent">
+                          <img
+                            src="/images/icons/interest3.png"
+                            className="proCricleImg"
+                            alt="range"
+                            width="80px"
+                          />
+                          <p className="text">Holiday Homes</p>
+                        </div>
+                      </Link>
+                      <div className="nbrCircle">
+                        <h6>04</h6>
+                      </div>
+                    </div>
+                    {/* Circle box End */}
+                    </SwiperSlide>
+                    <SwiperSlide className="lookingMobileCard">
+                      {/* Circle box Start */}
+                      <div className="proCircleCard mb-2">
+                        <Link className="proCircleBox lookingSingleMobileCard" href="/services">
+                          <div className="circleContent">
+                            <img
+                              src="/images/icons/interest4.png"
+                              className="proCricleImg"
+                              alt="range"
+                              width="80px"
+                            />
+                            <p className="text"> Mortgage </p>
+                          </div>
+                        </Link>
+                        <div className="nbrCircle">
+                          <h6>05</h6>
+                        </div>
+                      </div>
+                      {/* Circle box End */}
+                    </SwiperSlide>
 
+                    <div
+                      className="swiper-button-next text-primary"
+                      onClick={() => swiperRef.current?.slideNext()}
+                    >
+                      <span className="">
+                        <i className="bi bi-chevron-right fs-1"></i>
+                      </span>
+                    </div>
+                    <div
+                      className="swiper-button-prev text-primary"
+                      onClick={() => swiperRef.current?.slidePrev()}
+                    >
+                      <span className="">
+                        <i className="bi bi-chevron-left fs-1"></i>
+                      </span>
+                    </div>
+            </Swiper>
+          </div>
+            
+          
           <div className="col-12 col-lg-12 col-md-12">
             <div className="borderBottom"></div>
           </div>
