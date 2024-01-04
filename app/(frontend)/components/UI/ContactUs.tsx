41b77f6b2a73954ccae6e60511c3b2a5afa19d73
aqsa <aqsahussainphp@gmail.com>
2024-01-04 10:34:13 +0400

    about font update

--- app/(frontend)/components/UI/ContactUs.tsx
+++ app/(frontend)/components/UI/ContactUs.tsx
@@ -32,7 +32,7 @@ const ContactUs = () => {
     <div className="col-12 col-lg-4 col-md-4">
       <div className="text-start">
         <div className="mb-3">
-          <p className="text-dark mb-0"><b>CONTACT US</b></p>
+          <p className="text-dark mb-0"><b>WRITE TO US</b></p>
         </div>
 
         <div className="contactForm">
