893f7ddc41e03b0339ce4ce35249bd5d38738cd6
aqsa <romit@range.ae>
2024-02-14 18:20:47 +0400

    award gallery slider update

--- app/(frontend)/components/Award/page.tsx
+++ app/(frontend)/components/Award/page.tsx
@@ -2,13 +2,14 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import SwiperCore, { Swiper as SwiperType } from "swiper";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "@/public/css/management-styles.css";
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { useGetAllAwardData } from "@/src/services/AwardService";
 
 function AwardGallery(){
@@ -16,11 +17,11 @@ function AwardGallery(){
   const testimonialSwiperRef = useRef<SwiperCore>();
     return (
         <section className="section">
-          <h4 className="sctionMdTitle text-primary mb-5">
+          <h4 className="sctionMdTitle text-primary my-3">
             AWARD WINNING TEAM
           </h4>
           <Swiper
-            modules={[Navigation]}
+           
             onSwiper={(swiper) => {
               testimonialSwiperRef.current = swiper;
             }}
@@ -39,9 +40,14 @@ function AwardGallery(){
                 slidesPerView: 3,
               },
             }}
+            autoplay={{
+              delay: 3000,
+            }}
+            modules={[Navigation, Autoplay]}
             slidesPerView={1}
             navigation={true}
-            className="mySwiper multiItemsSlider"
+            spaceBetween={50}
+            className="mySwiper"
           >
             {AwardsData &&
               AwardsData?.map((award, index) => {
@@ -52,6 +58,22 @@ function AwardGallery(){
                 )
               })
             }
+            <div
+                          className="swiper-button-next text-white"
+                          onClick={() => testimonialSwiperRef.current?.slideNext()}
+                        >
+                          <span className="">
+                            <i className="bi bi-chevron-right fs-1"></i>
+                          </span>
+                        </div>
+                        <div
+                          className="swiper-button-prev text-white"
+                          onClick={() => testimonialSwiperRef.current?.slidePrev()}
+                        >
+                          <span className="">
+                            <i className="bi bi-chevron-left fs-1"></i>
+                          </span>
+                        </div>
           </Swiper>
         </section>
     );
