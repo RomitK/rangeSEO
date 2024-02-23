b746acd031ef354496ef84f5c03c669db6897ec0
romit <romit@range.ae>
2024-02-23 12:28:01 +0400

    google analytics

--- app/(frontend)/components/UI/Layout.tsx
+++ app/(frontend)/components/UI/Layout.tsx
@@ -2,16 +2,24 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import "react-datepicker/dist/react-datepicker.css";
-function Layout(props){
-    return (
-        <>
-        <Header/>
-        <Navbar/>
-        <section>
-            {props.children}
-        </section>
-        <Footer/>
-        </>
-    );
+
+function Layout(props) {
+  return (
+    <>
+      <Header />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PBWHF5MF"
+          height="0"
+          width="0"
+          className="hidden-iframe"
+        ></iframe>
+      </noscript>
+      <Navbar />
+      <section>{props.children}</section>
+      <Footer />
+    </>
+  );
 }
+
 export default Layout;
