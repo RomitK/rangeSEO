f1fb061c91b99acf2b2aa0d6556aba98394a3188
romit <romit@range.ae>
2024-02-23 15:13:24 +0400

    remove the anay

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -2,28 +2,28 @@ import type { Metadata } from "next";
 import Head from "next/head";
 import HomePage from "./components/home/homePage";
 
-// type Props = {
-//   params: { slug: string };
-//   searchParams: { [key: string]: string | string[] | undefined };
-// };
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-// export const generateMetadata = async ({
-//   params,
-// }: Props): Promise<Metadata> => {
-//   const HomeMeta = await fetch(`${process.env.API_HOST}homePage/meta`).then(
-//     (res) => res.json()
-//   );
-//   return {
-//     title: HomeMeta?.data?.title,
-//     description: HomeMeta?.data?.meta_description,
-//     keywords: HomeMeta?.data?.meta_keywords,
-//   };
-// };
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const HomeMeta = await fetch(`${process.env.API_HOST}homePage/meta`).then(
+    (res) => res.json()
+  );
+  return {
+    title: HomeMeta?.data?.title,
+    description: HomeMeta?.data?.meta_description,
+    keywords: HomeMeta?.data?.meta_keywords,
+  };
+};
 
 export default function Home() {
   return (
     <>
-      {/* <HomePage /> */}
+      <HomePage />
     </>
   );
 }
