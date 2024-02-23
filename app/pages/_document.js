fe2cb6538bf28de7cb02ee22df8e2f81120ec9c9
romit <romit@range.ae>
2024-02-23 15:20:38 +0400

    remove the anay

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,30 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import Script from 'next/script';
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Google Analytics script */}
-          <h1>fsdfsd</h1>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-ENW575XKY6"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-ENW575XKY6');
-              `,
-            }}
-          ></script>
+          {/* Add any other meta tags or head elements you need */}
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-ENW575XKY6`}
+            onLoad={() => {
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ENW575XKY6');
+            }}
+          />
         </body>
       </Html>
     );
