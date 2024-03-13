2ac542536353deaa33b251fe08384812fc87bb19
romit <romit@range.ae>
2024-03-13 12:02:50 +0400

    tag manager code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,23 +25,37 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
-<Script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"
-/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WFPP3R38');`,
+          }}
+        />
 
-<Script id="google-analytics">
-  {`
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"
+        />
+
+        <Script id="google-analytics">
+          {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
   `}
-</Script>
+        </Script>
 
-</head>
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
-      <Layout>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFPP3R38"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+
+        <Layout>
           {children}
           <ToastContainer
             position="bottom-center"
