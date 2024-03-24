c15c6acbe85dc95514a13f2868c085aa2c700253
romit <romit@range.ae>
2024-03-24 20:10:40 +0400

    layout comment for seo reverse

--- app/(frontend)/components/UI/Layout.tsx
+++ app/(frontend)/components/UI/Layout.tsx
@@ -2,13 +2,17 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import "react-datepicker/dist/react-datepicker.css";
+import { Suspense } from 'react';
+import Loader from "@/app/(frontend)/components/UI/Loader";
 
 function Layout(props) {
   return (
     <>
       <Header />
       <Navbar />
-      {/* <section>{props.children}</section> */}
+      <Suspense fallback={<Loader />}>
+        <section>{props.children}</section>
+      </Suspense>
       <Footer />
     </>
   );
