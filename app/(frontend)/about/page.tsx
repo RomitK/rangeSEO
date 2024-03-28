438333dfc47128c690aea36699be3ab4ea83fe46
romit <romit@range.ae>
2024-03-28 15:53:26 +0400

    meta working

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -3,40 +3,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import { SWRProvider } from "@/app/swr-provider";
 import Link from "next/link";
-import dynamic from 'next/dynamic';
-import type { Metadata } from "next";
-import PAGES from "@/src/constants/pages";
-const AboutPage = dynamic(() => import('@/app/(frontend)/components/AboutPage/AboutPage'));
-
-type Props = {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export const generateMetadata = async ({
-  params,
-}: Props): Promise<Metadata> => {
-  const meta = await fetch(
-    `${process.env.API_HOST}meta/${PAGES.about_the_range}`,
-    { cache: "no-store" }
-  )
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("err", err);
-    });
-  return {
-    title: meta?.data?.title,
-    description: meta?.data?.meta_description,
-    keywords: meta?.data?.meta_keywords,
-  };
-};
-
+import AboutPage from "../components/AboutPage/AboutPage";
 function About() {
   return (
     <>
       <SWRProvider>
-        <AboutPage />
-      </SWRProvider>
+      <AboutPage></AboutPage>
+    </SWRProvider>
     </>
   );
 }
--- app/(frontend)/services/page.tsx
+++ app/(frontend)/services/page.tsx
@@ -6,36 +6,8 @@ import { toast } from "react-toastify";
 import { saveContactFormApi } from "@/src/services/HomeService";
 import ContactModel from "../components/models/contactModel";
 import ServiceModel from '@/app/(frontend)/components/models/ServiceModel';
+
 import "@/public/css/services-styles.css";
-
-
-import dynamic from 'next/dynamic';
-import type { Metadata } from "next";
-import PAGES from "@/src/constants/pages";
-type Props = {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export const generateMetadata = async ({
-  params,
-}: Props): Promise<Metadata> => {
-  const meta = await fetch(
-    `${process.env.API_HOST}meta/${PAGES.services}`,
-    { cache: "no-store" }
-  )
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("err", err);
-    });
-  return {
-    title: meta?.data?.title,
-    description: meta?.data?.meta_description,
-    keywords: meta?.data?.meta_keywords,
-  };
-};
-
-
 function Services() {
   const router = useRouter();
   const [activeService, setActiveService] = useState('');
