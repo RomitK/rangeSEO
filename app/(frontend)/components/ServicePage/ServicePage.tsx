c4f2d82c38d5c687a127a0993692326dcf0f484c
romit <romit@range.ae>
2024-04-06 22:59:26 +0400

    service model class added

--- app/(frontend)/components/ServicePage/ServicePage.tsx
+++ app/(frontend)/components/ServicePage/ServicePage.tsx
@@ -69,7 +69,7 @@ const ServicePageContent = () => {
         // Push the event and data to the data layer
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
-            event: "serviceModelClik",
+            event: "serviceModelClick",
             serviceModelName: serviceName,
         });
     };
